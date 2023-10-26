adafbda4a18ebefc8d53b8b6486379183503e0b6
KARTHICK28 <karthick.r@NLPULTP7204778.FPS.NIHILENT.com>
2023-10-26 17:13:41 +0530

    New Changes

---
+++ LearningAutomation/src/main/java/ListenerDemo/Xmldemo.java
@@ -0,0 +1,14 @@
+package ListenerDemo;
+
+import org.testng.annotations.Test;
+
+public class Xmldemo {
+	
+	@Test
+	public void xmltest() {
+		
+		System.out.println("Hi!! Karthick I'm in");
+		
+	}
+
+}
---
+++ LearningAutomation/src/main/java/ListenerDemo/Xmldemo1.java
@@ -0,0 +1,16 @@
+package ListenerDemo;
+
+import org.testng.annotations.Test;
+
+public class Xmldemo1 {
+	
+	@Test
+	public void xmltest() {
+		
+		System.out.println("Hi!! Swathi I'm in");
+		
+	}
+
+}
+
+
