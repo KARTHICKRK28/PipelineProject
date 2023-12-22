10d266c676c4e7bab46e121a33a960fa9dee4637
KARTHICKRK28 <karthickrmd92@gmail.com>
2023-12-22 12:19:50 +0530

    Third Commit

---
+++ LearningAutomation/src/main/java/sampleExtentReport2/ExtendReportTestNG2.java
@@ -0,0 +1,35 @@
+package sampleExtentReport2;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtendReportTestNG2 {
+
+	public static void main(String[] args) {
+
+		// Blank html file
+		// (./)-->root of the project
+		ExtentSparkReporter report = new ExtentSparkReporter("C:\\Users\\karthick.r\\Videos\\Testreport.html");
+
+		// Actual report
+		ExtentReports extend = new ExtentReports();
+		extend.attachReporter(report);
+
+		ExtentTest test1 = extend.createTest("TC001_ Search product");
+		test1.pass("Enter the desired product name");
+		test1.pass("Check if the product is listed");
+		test1.pass("Add the product to the cart");
+
+		ExtentTest test2 = extend.createTest("TC002_ Making Payment");
+		test2.pass("Navigated to the payment page");
+		test2.fail("Payment got failed",
+				MediaEntityBuilder.createScreenCaptureFromPath("failed.png").build());
+
+		// Last
+		extend.flush();
+		System.out.println("Products are listed and added Successfully");
+
+	}
+}
