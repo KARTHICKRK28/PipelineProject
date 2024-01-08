5cd07ec7844da70addcf838de4cbe81d26a9002b
KARTHICKRK28 <karthickrmd92@gmail.com>
2024-01-08 16:59:37 +0530

    Sixth Commit

---
+++ LearningAutomation/src/main/java/pages/HomePage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import java.sql.DriverManager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+	WebDriver driver;
+
+	public HomePage(WebDriver driver) {
+		// TODO Auto-generated method stub
+		this.driver = driver;
+
+	}
+
+	// Locator for login button
+	By LoginBtn = By.id("login");
+
+	// Method to click login button
+	public void clickLogin() {
+		driver.findElement(LoginBtn).click();
+	}
+
+}
